1912373977b683e3ea02956377e87254ecdf062d
larissa <larissa.slomsky@gmail.com>
2024-04-22 19:52:30 -0300

    adicionando mensagem padrão caso um número negativo seja colocado no input

--- client/src/PrimeNumbersInput.js
+++ client/src/PrimeNumbersInput.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import './styles/PrimeNumbersInput.css';
 
 
 function PrimeNumbersInput() {
@@ -19,30 +20,42 @@ function PrimeNumbersInput() {
     };
 
     const getPrimeNumbersAndCalculatePerformance = async () => {
-        const start = performance.now();
-        const r = await fetchData()
-        setAnswer(r)
-        setInputValue('')
-        const timeTaken = performance.now() - start;
-        setTime('tempo em ms para execução: ' + timeTaken)
+        console.log(isNegativeNumber())
+        if (isNegativeNumber()) {
+            setAnswer('Insira um número positivo.')
+            setInputValue('')
+            setTime('')
+        } else {
+            const start = performance.now();
+            const r = await fetchData()
+            setAnswer(`A quantidade de números primos até ${inputValue} é: ${r} `)
+            const timeTaken = (performance.now() - start) / 1000; 
+            setTime('Tempo para execução: ' + timeTaken.toFixed(2) + ' segundos.'); 
+        }
+        
+    }
+
+    let isNegativeNumber = () => {
+        return inputValue < 0 ? true : false
     }
     
     return (
-        <div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
             <div>
                 <input
-                id='inpt'
-                type="number"
-                value={inputValue}
-                onChange={handleInputChange}
-                placeholder="Digite um número inteiro"
+                    id='inpt'
+                    type="number"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder="Digite um número inteiro"
+                    style={{ width: '300px', padding: '10px', marginBottom: '20px' }}
                 />
-                <button onClick={getPrimeNumbersAndCalculatePerformance} id='btn'>Enviar</button>
+                <button onClick={getPrimeNumbersAndCalculatePerformance} id='btn' style={{ padding: '10px 20px', backgroundColor: '#007bff', color: 'white', border: 'none', cursor: 'pointer' }}>Enviar</button>
             </div>
-        <div>
-            <h1>{answer}</h1>
-            <h1>{time}</h1>
-        </div>
+            <div>
+                <h1 style={{ textAlign: 'center' }}>{answer}</h1>
+                <h1 style={{ textAlign: 'center' }}>{time}</h1>
+            </div>
         </div>
     );
 }
