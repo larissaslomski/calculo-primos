6af58ac96e4c18d56a26522b6ee3d397e3f9c743
larissa <larissa.slomsky@gmail.com>
2024-04-21 21:58:51 -0300

    mudança de nome no componente de app para PrimeNumbersInput

---
+++ client/src/components/inputCard.js
@@ -0,0 +1,46 @@
+
+export default function InputText ({id, inputValue, rotulo, valorPadrao, referencia}) {
+    const [inputValue, setInputValue] = useState('');
+    const [answer, setAnswer] = useState('');
+    const [time, setTime] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const fetchData = () => {
+        return fetch(`/primes/${inputValue}`)
+        .then(response => response.json())
+        .catch(error => {
+            console.error('Erro ao enviar requisição:', error);
+        });
+    };
+
+    const getPrimeNumbersAndCalculatePerformance = async () => {
+        const start = performance.now();
+        const r = await fetchData()
+        setAnswer(r)
+        setInputValue('')
+        const timeTaken = performance.now() - start;
+        setTime('tempo em ms para execução: ' + timeTaken)
+    }
+    
+    return (
+        <div>
+        <div>
+            <input
+            id={id}
+            type="number"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Digite um número inteiro"
+            />
+            <button onClick={getPrimeNumbersAndCalculatePerformance} id='btn'>Enviar</button>
+        </div>
+        <div>
+            <h1>{answer}</h1>
+            <h1>{time}</h1>
+        </div>
+        </div>
+    );
+}
